0649de81e666c866ace78fc638e8171d32c07fc4
Hassan Ibrahim <hassan2002_kimo@hotmail.com>
2025-04-16 10:10:53 +0000

    0x01 added tasks 2-9

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled(promises).then((results) => {
+    const array = [];
+    results.forEach((result) => array.push({ status: result.status, value: result.value }));
+  });
+}
