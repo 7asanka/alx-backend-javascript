a7c576d0f6a8a0871aa7917d3457e3d507310c6f
Hassan Ibrahim <hassan2002_kimo@hotmail.com>
2025-04-16 10:22:02 +0000

    0x01 tasks fixes

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,10 +2,10 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((result) => {
-      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
-    .catch((error) => {
-      console.log('Signup system offline', error);
+    .catch(() => {
+      console.log('Signup system offline');
     });
 }
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,22 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises).then((results) => {
-    const array = [];
-    results.forEach((result) => array.push({ status: result.status, value: result.value }));
-  });
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => {
+      results.map((result) => {
+        if (result.status === 'fulfilled') {
+          return {
+            status: result.status,
+            value: result.value,
+          };
+        }
+        return {
+          status: result.status,
+          value: result.reason,
+        };
+      });
+    });
 }
